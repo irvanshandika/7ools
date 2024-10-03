430bcf5fc43a44c4598a3c7efdbbebe17dff58f8
irvanshandika <shandikamuhammadirvan@gmail.com>
2024-10-03 11:44:30 +0700

    Adding layout

---
+++ src/Layout.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Hotjar from "~/src/components/Hotjar.vue";
+
+useSeoMeta({
+  description: "7ools: Perangkat Produktivitas All-in-One untuk Profesional Modern",
+  categories: ["productivity", "tools", "all-in-one", "modern", "professional", "7ools"],
+  ogDescription: "7ools: Perangkat Produktivitas All-in-One untuk Profesional Modern",
+  ogType: "website",
+  ogUrl: "https://7ools.vercel.app",
+  ogImage:
+    "https://ogcdn.net/2c2c6737-47d4-4459-9969-e711eb48394c/v1/7ools.vercel.app/Home%20%7C%207ools/7ools%3A%20Perangkat%20Produktivitas%20All-in-One%20untuk%20Profesional%20Modern/https%3A%2F%2Fcdn.theopengraph.com%2Fproduction%2Fdocuments%2Fd564a463-1c51-4259-991d-0d17bf3e0391.jpg%3Ftoken%3D-hKrjcO4EFDmPh0lgxSanJdH2MZ9gYnfw4V87WUckbA%26height%3D800%26width%3D1200%26expires%3D33239188519/og.png",
+  // Twitter
+  twitterCard: "summary_large_image",
+  twitterTitle: "Home | 7ools",
+  twitterDescription: "7ools: Perangkat Produktivitas All-in-One untuk Profesional Modern",
+  twitterImage:
+    "https://ogcdn.net/2c2c6737-47d4-4459-9969-e711eb48394c/v1/7ools.vercel.app/Home%20%7C%207ools/7ools%3A%20Perangkat%20Produktivitas%20All-in-One%20untuk%20Profesional%20Modern/https%3A%2F%2Fcdn.theopengraph.com%2Fproduction%2Fdocuments%2Fd564a463-1c51-4259-991d-0d17bf3e0391.jpg%3Ftoken%3D-hKrjcO4EFDmPh0lgxSanJdH2MZ9gYnfw4V87WUckbA%26height%3D800%26width%3D1200%26expires%3D33239188519/og.png",
+  twitterUrl: "https://7ools.vercel.app",
+  twitterDomain: "7ools.vercel.app",
+});
+</script>
+
+<template>
+  <Hotjar />
+  <slot />
+</template>
---
+++ src/components/Hotjar.vue
@@ -0,0 +1,8 @@
+<script>
+import Hotjar from "@hotjar/browser";
+
+const siteId = 5157324;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
+</script>
