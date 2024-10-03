67f4a04fec85fe6edf69f7468802ab96330313cb
irvanshandika <shandikamuhammadirvan@gmail.com>
2024-10-03 12:03:55 +0700

    Trying fix hotjar 2

--- plugins/hotjar.client.ts
+++ plugins/hotjar.client.ts
@@ -6,12 +6,25 @@ export default defineNuxtPlugin(() => {
   const hotjarId = runtimeConfig.public.hotjarId
 
   if (process.client && hotjarId) {
-    window.hj = window.hj || function(){(window.hj.q = window.hj.q || []).push(arguments)};
-    window._hjSettings = { hjid: hotjarId, hjsv: 6 };
     const script = document.createElement('script');
-    script.async = true;
-    script.src = `https://static.hotjar.com/c/hotjar-${hotjarId}.js?sv=6`;
-    document.head.appendChild(script);
+    script.innerHTML = `
+      (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:${hotjarId},hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    `;
+    
+    // Pastikan script dimasukkan segera setelah tag <head>
+    const head = document.head;
+    if (head.firstChild) {
+      head.insertBefore(script, head.firstChild);
+    } else {
+      head.appendChild(script);
+    }
   }
 })
 
