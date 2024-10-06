aeb47d390fa3863107f85f9134d6a83bc10ada2f
irvanshandika <shandikamuhammadirvan@gmail.com>
2024-10-06 13:51:05 +0700

    Change code client

--- plugins/gtm.client.ts
+++ plugins/gtm.client.ts
@@ -1,23 +1,21 @@
 import { defineNuxtPlugin } from "#app";
 
 export default defineNuxtPlugin(() => {
-  const GTM_ID = "G-XX7JF4WM7H"; // Ganti dengan ID GTM Anda
-
-  function gtag(...args: any[]) {
-    // @ts-ignore
-    window.dataLayer.push(arguments);
-  }
-
-  // @ts-ignore
-  window.dataLayer = window.dataLayer || [];
-  gtag("js", new Date());
-  gtag("config", GTM_ID);
+  const GA4_ID = "G-XX7JF4WM7H"; // Ganti dengan ID GA4 Anda
 
   useHead({
     script: [
       {
-        src: `https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`,
+        src: `https://www.googletagmanager.com/gtag/js?id=${GA4_ID}`,
         async: true,
+      },
+      {
+        innerHTML: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA4_ID}');
+        `,
       },
     ],
   });
